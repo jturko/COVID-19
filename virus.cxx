82043a249cd70a19b3121a3868f19e9c4fbab5eb
jturko <jturko@mail.uoguelph.ca>
2020-04-01 19:16:54 -0400

    Added phase-in/phase-out of quarantine period

--- virus.cxx
+++ virus.cxx
@@ -3,22 +3,32 @@
 // bit to do with the social distancing everyone is talking about 
 // now due to the coronavirus
 //
+// compile with:
+// g++ `root-config --cflags --glibs` virus.cxx
+// 
 // joey turko, 20/3/2020
+//
 
 #include<iostream>
 #include<cmath>
 #include<fstream>
 
+#include "TH1F.h"
+#include "THStack.h"
+#include "TLine.h"
+#include "TCanvas.h"
+
 double xdim = 150;
 double ydim = 100;
-const int npeople = 200;
-double infectionlength = 200;
+const int npeople = 250;
+double infectionlength = 300;
 double socialdistancing = 0.9;
 double stepsize = 0.5;
 double contactsize = 3.0;
-double quarantinetime = 600;
+double quarantinetime = 1000;
 double changedirectioninc = 0.2;
-double infectionprobability = 0.05;
+double infectionprobability = 0.03;
+int casesuntilquarantine = 20;
 
 enum condition
 {
@@ -51,7 +61,58 @@ void printparameters() {
              <<"social dist. duration:\t"<<quarantinetime<<std::endl
              <<"social dist. factor:\t"<<socialdistancing<<std::endl
              <<"infection prob.:\t"<<infectionprobability<<std::endl
+             <<"cases pre-quarantine:\t"<<casesuntilquarantine<<std::endl
              <<"====================================="<<std::endl;
+}
+
+void plot(double starttime=0, double endtime=0)
+{
+    TCanvas * c1 = new TCanvas();
+    std::ifstream infile;
+    infile.open("output.dat");
+
+    const int max_size = 10000;
+    int size = 0;
+    int time[max_size];
+    int sick[max_size];
+    int healthy[max_size];
+    int immune[max_size];
+    while(!infile.eof()) {
+        infile >> time[size] >> sick[size] >> healthy[size] >> immune[size];
+        size++;
+    }
+
+    TH1F * hs = new TH1F("sick","sick",size,0,size);
+    hs->SetFillColor(kMagenta-4);
+    hs->GetYaxis()->SetRangeUser(0,npeople);
+
+    TH1F * hh = new TH1F("healthy","healthy",size,0,size);
+    hh->SetFillColor(kGray);
+    hh->GetYaxis()->SetRangeUser(0,npeople);
+
+    TH1F * hi = new TH1F("immune","immune",size,0,size);
+    hi->SetFillColor(kTeal-4);
+    hi->GetYaxis()->SetRangeUser(0,npeople);
+
+    for(int i=0; i<size; i++) {
+        hs->SetBinContent(i,sick[i]);
+        hh->SetBinContent(i,healthy[i]);
+        hi->SetBinContent(i,immune[i]);
+    }
+    THStack * stack = new THStack("stack","stack");
+    stack->Add(hs);
+    stack->Add(hh);
+    stack->Add(hi);
+    hi->Draw();
+    stack->Draw("same");
+    stack->SetMaximum(npeople);
+
+    TLine * qs = new TLine(starttime,0,starttime,npeople);
+    TLine * qe = new TLine(endtime,0,endtime,npeople);
+    qs->Draw("same");
+    qe->Draw("same");
+
+    c1->SaveAs("plot.pdf");
 }
 
 class person
@@ -130,7 +191,11 @@ int main() {
     int nsick = 1;
     int nimmune = 0;
     double time = 0.;
-    
+    double currentsocialdistancing = 0.;    
+    double quarantinestarttime = 1000000;
+    bool quarantinestarted = false;
+    bool quarantinefinished = false;
+
     // populate array with healthy people at random positions
     person * people[npeople];
     for(int i=0; i<npeople; i++) people[i] = new person(i);
@@ -141,8 +206,18 @@ int main() {
 
     while(nimmune < npeople && nsick > 0) // this iterator is the passage of time
     {
-        if(time > quarantinetime) socialdistancing = 0.;
-        for(int i=0; i<npeople*(1.0-socialdistancing); i++) people[i]->walk(); // make them walk
+        if(nsick >= casesuntilquarantine && quarantinestarted==false) { 
+            std::cout<<"---> quarantine started!"<<std::endl;
+            currentsocialdistancing = socialdistancing;
+            quarantinestarttime = time;
+            quarantinestarted = true;
+        }
+        if((time-quarantinestarttime) > quarantinetime && quarantinefinished==false) { 
+            std::cout<<"---> quarantine ended!"<<std::endl;
+            currentsocialdistancing = 0.;
+            quarantinefinished = true;
+        }
+        for(int i=0; i<npeople*(1.0-currentsocialdistancing); i++) people[i]->walk(); // make them walk
         
         for(int i=0; i<npeople; i++) // loop over all people
         {
@@ -175,7 +250,7 @@ int main() {
             people[i]->changedirection();
         }
         time += 1.;
-        std::cout<<std::flush<<"time: "<<time<<", nsick: "<<nsick<<", nhealthy: "<<nhealthy<<", nimmune: "<<nimmune<<"                \r";
+        std::cout<<"\rtime: "<<time<<", nsick: "<<nsick<<", nhealthy: "<<nhealthy<<", nimmune: "<<nimmune<<"  "<<std::flush;
         outfile<<time<<"\t"<<nsick<<"\t"<<nhealthy<<"\t"<<nimmune<<std::endl;
 
         // save data for making a gif
@@ -188,4 +263,7 @@ int main() {
     }
     outfile.close();
     std::cout<<std::endl;
+    
+    plot(quarantinestarttime,quarantinestarttime+quarantinetime);
 }
+
